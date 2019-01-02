e0b1daa43a88e4d9e8506a327cb13310f7a49f6c
Paula McKinley <pmknyc@optimum.net>
2019-01-02 16:58:44 +0000

    custom error coded

--- custom_errors.rb
+++ custom_errors.rb
@@ -22,7 +22,8 @@ class Person
   class PartnerError < StandardError
   	def message 
 	    "you must give the get_married method an argument of an instance of the person class!"
-	  end
+	    "Custom Error practice is a success!"
+	   end
 	  
   end # PartnerError end
 end # Person class end
